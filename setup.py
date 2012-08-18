84789685dba02ea8fb89d56d0e4a089a4b6779cb
Максим Коринец <mkorinets@gmail.com>
2012-08-18 16:10:48 +0400

    смена версии на 0.6.7-krew

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.version_info[:3] < (2,5,0):
     requires.append('pysqlite')
 
 setup(name='kook',
-      version='0.6.5-templ',
+      version='0.6.7-krew',
       description='a robust recipe inventory storing and sharing',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
