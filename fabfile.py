469ab1ce0dbec87faf89ced776b6b34f062018d7
Максим Коринец <mkorinets@gmail.com>
2013-05-10 07:46:48 +0400

    removed fabfile from index

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,26 @@
 from fabric.api import *
 
+APP_NAME = 'kook'
+env.hosts = ['ubuntu@ec2-54-217-246-51.eu-west-1.compute.amazonaws.com']
+env.key_filename = ['/home/yentsun/.ssh/yentsunkey.pem']
+
 
 def pack():
-    local('python setup.py sdist --formats=gztar', capture=False)
+    local('~/env/bin/python setup.py sdist --formats=gztar', capture=False)
+
+
+def deploy():
+    dist = local('~/env/bin/python setup.py --fullname', capture=True).strip()
+    put('dist/{dist}.tar.gz'.format(dist=dist),
+        '{dist}.tar.gz'.format(dist=dist))
+    run('tar xzf {dist}.tar.gz'.format(dist=dist))
+    run('rm -f {dist}.tar.gz'.format(dist=dist))
+    with cd('{dist}'.format(dist=dist)):
+        run('~/env/bin/python setup.py install')
+    with cd(APP_NAME):
+        run('supervisorctl shutdown')
+    run('rm -rf {appname}'.format(appname=APP_NAME))
+    run('mv {dist} {appname}'.format(appname=APP_NAME, dist=dist))
+    with cd(APP_NAME):
+        run('supervisord')
+        run('supervisorctl status')
